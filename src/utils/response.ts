5fb3e0dbeb60d67c93be2068d6fae8c39198ee86
nasmy <nasmy@keeneye.solutions>
2022-05-22 02:27:18 +0530

    Modification
    change description in README.md
    Change response.ts remove comment

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,14 +1,3 @@
-
-/**
- * Api Response Format
- * @param {*} success 
- * @param {*} code 
- * @param {*} data 
- * @param {*} message 
- * @param {*} error 
- * @returns {object}
- */
-
 export const commonResponse = (success: any = false, code: any, data: any = {}, message: any = "", error: any = "") => {
     return {
         success: success,
